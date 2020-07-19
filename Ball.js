7c4ac8dfee7d6652182200710354c8aaff4f262d
Charvi04 <66305846+Charvi04@users.noreply.github.com>
2020-07-19 19:37:41 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,23 @@
+class Ball {
+    constructor(x,y,r) {
+      var options = {
+          isStatic:false,
+          density:1.0,
+          restitution:0.5,
+          friction:1.6,
+          stiffness:0.04
+    }
+      this.body = Bodies.circle(x,y,r,options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("Images/paper.png")
+      World.add(world, this.body);
+    }
+    display(){
+      
+        
+      imageMode(CENTER);
+     
+      image(this.image,this.body.position.x,this.body.position.y,60,60);
+    }
+  };
